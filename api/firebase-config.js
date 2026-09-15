e5b859990c04a8cfcc1aaa7b1fae6f0dd4d6a0bc
marlyhk <db9bbbykz9@privaterelay.appleid.com>
2026-09-15 17:50:39 +0300

    Update firebase-config.js

--- api/firebase-config.js
+++ api/firebase-config.js
@@ -1,17 +1,31 @@
 module.exports = function handler(req, res) {
-  const projectId = process.env.PLANIT_FIREBASE_PROJECT_ID || '';
+  const projectId =
+    process.env.PLANIT_FIREBASE_PROJECT_ID || '';
 
   const config = {
-    apiKey: process.env.PLANIT_FIREBASE_API_KEY || '',
-    authDomain: projectId ? `${projectId}.firebaseapp.com` : '',
+    apiKey:
+      process.env.PLANIT_FIREBASE_API_KEY || '',
+
+    authDomain:
+      projectId ? `${projectId}.firebaseapp.com` : '',
+
     projectId,
-    appId: process.env.PLANIT_FIREBASE_APP_ID || ''
+
+    appId:
+      process.env.PLANIT_FIREBASE_APP_ID || ''
   };
 
-  res.setHeader('Content-Type', 'application/javascript; charset=utf-8');
-  res.setHeader('Cache-Control', 'no-store, max-age=0');
+  res.setHeader(
+    'Content-Type',
+    'application/javascript; charset=utf-8'
+  );
 
-  res
-    .status(200)
-    .send(`window.PLANIT_FIREBASE_CONFIG = ${JSON.stringify(config)};`);
+  res.setHeader(
+    'Cache-Control',
+    'no-store, max-age=0'
+  );
+
+  res.status(200).send(
+    `window.PLANIT_FIREBASE_CONFIG = ${JSON.stringify(config)};`
+  );
 };
